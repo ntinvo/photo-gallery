3c8b92ed23ba6c5d15afa37942ac0c7275cf4f1f
Tin Vo <tinnvo1101@gmail.com>
2018-10-20 23:30:02 -0400

    added a very basic photo gallery component

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Photo Gallery
+
+[![Build Status](https://travis-ci.com/tinnvo/photo-gallery.svg?branch=master)](https://travis-ci.com/tinnvo/photo-gallery)
---
+++ src/components/PhotoGallery.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Lessons extends Component {
+  render() {
+    return (
+      <div>
+        Photo Gallery
+      </div>
+    );
+  }
+}
+
+export default Lessons;
