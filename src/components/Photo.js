5150c4c282dedc3ed8756b8c1769d8a36bf31f72
Tin Vo <tinnvo1101@gmail.com>
2018-10-21 17:46:19 -0400

    added an initial state the the app - this includes a list of image urls and the current index (start at 0)

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const Photo = (props) => {
+  const photo = {
+    backgroundImage: `url(${props.property.image})`,
+    key: `${props.property.key}`
+  }
   return (
-    <div className="photo">Photo Components</div>
+    <div className="photo" style={photo}></div>
   )
 }
 
--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -5,6 +5,27 @@ import RightArrow from './arrows/RightArrow.js';
 
 class Lessons extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [
+        "https://images.pexels.com/photos/36487/above-adventure-aerial-air.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "https://images.pexels.com/photos/956981/milky-way-starry-sky-night-sky-star-956981.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "https://images.pexels.com/photos/292442/pexels-photo-292442.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "https://images.pexels.com/photos/33045/lion-wild-africa-african.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "https://images.pexels.com/photos/324028/pexels-photo-324028.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "https://images.pexels.com/photos/218983/pexels-photo-218983.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "https://images.pexels.com/photos/955463/pexels-photo-955463.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "https://images.pexels.com/photos/1308624/pexels-photo-1308624.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "https://images.pexels.com/photos/17679/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "https://images.pexels.com/photos/434213/pexels-photo-434213.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "https://images.pexels.com/photos/6347/coffee-cup-working-happy.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      ],
+      currentIndex: 0
+    }
+  }
+
   leftPhoto = () => {
     console.log("go left");
   }
@@ -14,10 +35,13 @@ class Lessons extends Component {
   }
 
   render() {
+    let property = {
+      image: this.state.images[this.state.currentIndex],
+      key: this.state.currentIndex
+    }
     return (
       <div>
-        Photo Gallery
-        <Photo />
+        <Photo property={property} />
         <LeftArrow leftPhoto={this.leftPhoto} />
         <RightArrow rightPhoto={this.rightPhoto} />
       </div>
