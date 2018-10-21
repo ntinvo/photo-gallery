7ec18ccadc38010742df38abbb66854de6bd5466
Tin Vo <tinnvo1101@gmail.com>
2018-10-21 17:30:43 -0400

    added Photo components

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Photo from './Photo.js';
 
 class Lessons extends Component {
   render() {
     return (
       <div>
         Photo Gallery
+        <Photo />
       </div>
     );
   }
